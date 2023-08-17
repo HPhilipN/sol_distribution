79c8d1b2fb70b695b66804df30153aa667ab2013
Phillip Nguyen <73822780+HPhilipN@users.noreply.github.com>
2023-08-17 11:31:02 -0700

    changed file stuff

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p> GUH </p>
+
+<script>
+    let count = 0
+    function increment(){
+        count += 1
+    }
+</script>
+
+<p>Clicked {count} {count === 1 ? 'time' : 'times'}</p>
+<button on:click={increment}>Click</button>
